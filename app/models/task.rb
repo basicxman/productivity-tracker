57e216115a90289243135d39f1306346fd843034
Andrew Horsman <minirobotics@gmail.com>
2011-10-19 22:41:36 -0400

    Iteratively deducts points.

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,11 +4,32 @@ class Task < ActiveRecord::Base
 
   validates_numericality_of :required_points, :greater_than => 0
 
+  def iterative_deduction(value, points = [])
+    return points if value == 0
+
+    point_id = self.points.joins(:day).reorder("date DESC").where("value > 0").limit(1)
+    return points if point_id.nil? or point_id.length == 0
+
+    point = Point.find(point_id.first.id)
+    temp = point.value
+
+    if temp - value < 0
+      point.update_attribute(:value, 0)
+      return iterative_deduction(value - temp, points + [point])
+    else
+      point.update_attribute(:value, temp - value)
+      return points + [point]
+    end
+  end
+
   def add_points(value)
     point = self.points.find_or_create_by_day_id(user.days.find_by_date(Date.today).id)
     point.value ||= 0
-    if value < 0 and point.value == 0
-      point = Point.find(self.points.joins(:day).reorder("date DESC").where("value > 0").limit(1).first.id)
+    if value < 0
+      point.save
+      puts "Saved point."
+      puts "Starting iterative deduction"
+      return iterative_deduction(-value)
     end
 
     temp = self.points.sum("value") + value
